f9b7ba8d866c01e1918aeabb4760af81411c329c
ketanbhaliya <ketanbhaliya33@gmail.com>
2026-03-14 20:16:59 +0530

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pickle
+
+st.title("Iris Prediction")
+
+# load model
+with open("model_svm.pkl", "rb") as f:
+    model = pickle.load(f)
+
+sl = st.slider("Sepal Length", 2.0, 10.0)
+sw = st.slider("Sepal Width", 2.0, 10.0)
+pl = st.slider("Petal Length", 2.0, 10.0)
+pw = st.slider("Petal Width", 2.0, 10.0)
+
+if st.button("Predict"):
+    prediction = model.predict([[sl, sw, pl, pw]])
+    st.success(prediction[0])
